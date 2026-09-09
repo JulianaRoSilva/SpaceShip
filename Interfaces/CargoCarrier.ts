37562288186b61abd47412ed8f123a21c1654200
Juliana <rsilvajuju@gmail.com>
2026-09-09 19:51:11 -0300

    teste

---
+++ Interfaces/CargoCarrier.ts
@@ -0,0 +1,10 @@
+
+
+export interface CargoCarrier {
+ 
+    loadCargo(amount: number): void;
+    unloadCargo(amount: number): void;
+    getCargoCapacity(): number;
+    getCurrentCargo(): number;
+
+}
